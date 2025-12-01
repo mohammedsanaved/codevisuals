80dd20df7d1e8c8df998f7f77e675fb19bee051e
mohammedsanaved <mosanaved@gmail.com>
2025-12-01 13:36:20 +0530

    changes in TabName

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
-import Footer from "@/components/Footer";
-import { Toaster } from "react-hot-toast";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import ConvexClientProvider from '@/components/providers/ConvexClientProvider';
+import Footer from '@/components/Footer';
+import { Toaster } from 'react-hot-toast';
 import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Code Craft",
-  description: "Share and run code snippets",
+  title: 'Code Visual',
+  description: 'Share and run code snippets',
 };
 
 export default function RootLayout({
@@ -30,14 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang='en'>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col`}
         >
           <ConvexClientProvider>
             {children}
-          <Analytics />
-
+            <Analytics />
           </ConvexClientProvider>
 
           <Footer />
